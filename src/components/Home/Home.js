492aeb0d5973f9c2a80cdec612ef6ab806747b4c
Dylan Fox <dylan.fox.kc@gmail.com>
2022-04-29 21:44:17 -0500

    add start of home section

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Dylan Fox
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
