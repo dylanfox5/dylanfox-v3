e8c39be3bf46115739c279cfe51b997246148d21
Dylan Fox <dylan.fox.kc@gmail.com>
2022-05-07 22:15:53 -0500

    Updating home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,21 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button';
 import './Home.css';
 
 function Home() {
   return (
-    <div className="Home">
-      <header className="Home-header">
-        <p>
-          Dylan Fox
-        </p>
-      </header>
-    </div>
+    <Container className="Home">
+        <Row>
+            <Col sm={12} lg={6} className="border">
+                <h1 className="home-title">dylan fox</h1>
+            </Col>
+            <Col sm={12} lg={6} className="border">
+                <h1>dylan-fox</h1>    
+            </Col>
+        </Row>
+    </Container>
   );
 }
 
