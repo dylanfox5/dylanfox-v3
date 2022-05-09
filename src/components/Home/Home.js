7f65beb0caad1b2d9c1376ac389da62ff76e0e09
Dylan Fox <dylan.fox.kc@gmail.com>
2022-05-08 20:02:35 -0500

    wrap up projects and add contact

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
     <Container className="Home" id="home">
       <Row>
         <Col sm={12} lg={8} className="animate__animated animate__fadeInLeft animate__delay-1s">
-          <h3>Hi, I'm <span>Dylan</span> -- an analytics focused developer who enjoys innovating solutions for complex problems.</h3>
+          <h3>Hi, I'm <span>Dylan</span> -- an analytics focused software developer who enjoys innovating solutions for complex problems.</h3>
         </Col>
         <Col sm={12} lg={4}></Col>
       </Row>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
-import { Folder2 } from 'react-bootstrap-icons';
+import { Folder2, BoxArrowUpRight } from 'react-bootstrap-icons';
 import './Projects.css';
 
 function Projects() {
@@ -11,43 +11,78 @@ function Projects() {
             <Row>
                 <h1 className='projects-title'>projects</h1>
                 <Col xs={12} md={6} lg={4}>
-                    <Card>
-                        <Card.Body>
-                            <Folder2 className="icon" />
-                            <Card.Text as="h5">
-                                v2
-                            </Card.Text>
-                            <Card.Text>
-                                The second interation of my porfolio website. Used React, React-Bootstrap, JS, jQuery, HTML & CSS 
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Row className="mx-1">
+                        <Card>
+                            <Card.Body>
+                                <a target="_blank" href="https://github.com/dylanfox5/dylanfox-v3"><Folder2 className="folder-icon" /></a>
+                                <Card.Text as="h5">
+                                    v3
+                                </Card.Text>
+                                <Card.Text>
+                                    The third interation of my porfolio website
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                    <Row className="mx-1">
+                        <Card>
+                            <Card.Body>
+                                <a target="_blank" href="https://github.com/dylanfox5/dylanfox-v1"><Folder2 className="folder-icon" /></a>
+                                <Card.Text as="h5">
+                                    v1
+                                </Card.Text>
+                                <Card.Text>
+                                    The first iteration of my porfolio website
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
                 </Col>
                 <Col xs={12} md={6} lg={4}>
-                    <Card>
-                        <Card.Body>
-                            <Folder2 className="icon" />
-                            <Card.Text as="h5">
-                                v1
-                            </Card.Text>
-                            <Card.Text>
-                                The first interation of my porfolio website. Used JS, jQuery, HTML & CSS, Bootstrap
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Row className="mx-1">
+                        <Card>
+                            <Card.Body>
+                                <a target="_blank" href="https://github.com/dylanfox5/v2-migration"><Folder2 className="folder-icon" /></a>
+                                <Card.Text as="h5">
+                                    v2-react
+                                </Card.Text>
+                                <Card.Text>
+                                    The second interation of my porfolio website using React
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                    <Row className="mx-1">
+                        <Card>
+                            <Card.Body>
+                                <a target="_blank" href="https://github.com/dylanfox5/graceland-today"><Folder2 className="folder-icon" /></a>
+                                <Card.Text as="h5">
+                                    Graceland Today
+                                </Card.Text>
+                                <Card.Text>
+                                    Conceptualized and built an online calendar for Graceland University. It contained daily, weekly, & monthly events
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
                 </Col>
                 <Col xs={12} md={6} lg={4}>
-                    <Card>
-                        <Card.Body>
-                            <Folder2 className="icon" />
-                            <Card.Text as="h5">
-                                Foxpad
-                            </Card.Text>
-                            <Card.Text>
-                                Conceptualized and built an online calendar for Graceland University. It contained daily, weekly, & monthly events.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <Row className="mx-1">
+                        <Card>
+                            <Card.Body>
+                                <a target="_blank" href="https://github.com/dylanfox5/dylanfox-v2"><Folder2 className="folder-icon" /></a>
+                                <Card.Text as="h5">
+                                    v2
+                                </Card.Text>
+                                <Card.Text>
+                                    The second interation of my porfolio website
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                    <Row className="mx-1">
+
+                    </Row>
                 </Col>
             </Row>
         </Container>
